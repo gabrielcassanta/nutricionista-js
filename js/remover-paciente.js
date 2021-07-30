7bc9b36f88df8e40907223bb2e5dcb82e51b5184
Gabriel Cassanta <cassantagabriel@gmail.com>
2021-07-30 11:53:07 -0300

    esmaecer

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,8 +1,8 @@
-var pacientes = document.querySelectorAll(".paciente");
-
 var tabela = document.querySelector("table");
 
-tabela.addEventListener("dblclick", function(event){
-  console.log(event.target);
-  event.target.remove();
+tabela.addEventListener("dblclick", function (event) {
+  event.target.parentNode.classList.add("fadeOut");
+  setTimeout(() => {
+    event.target.parentNode.remove();
+  }, 1000);
 });
